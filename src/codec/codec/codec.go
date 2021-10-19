48dc7c82239d8470a182aa12cbd39fdcfabf889b
zhouzijie <zhouzijie@didiglobal.com>
2021-10-19 21:20:48 +0800

    [add] 提交codec代码

--- src/codec/codec/codec.go
+++ src/codec/codec/codec.go
@@ -2,8 +2,31 @@ package codec
 
 import "io"
 
-type Header struct{
+type Header struct {
 	ServiceMethod string // format "Service.Method"
-	Seq uint64 //请求的序号，用来区分不同的请求
-	Error string
+	Seq           uint64 // 请求的序号，用来区分不同的请求
+	Error         string
+}
+
+type Codec interface {
+	io.Closer
+	ReadHeader(*Header) error
+	ReadBody(interface{}) error
+	Write(*Header, interface{}) error
+}
+
+type NewCodecFunc func(io.ReadWriteCloser) Codec
+
+type Type string
+
+const (
+	GobType  Type = "application/gob"
+	JsonType Type = "applicaion/json" // not implemented
+)
+
+var NewCodecFuncMap map[Type]NewCodecFunc
+
+func init() {
+	NewCodecFuncMap = make(map[Type]NewCodecFunc)
+	NewCodecFuncMap[GobType] = NewGobCodec
 }
